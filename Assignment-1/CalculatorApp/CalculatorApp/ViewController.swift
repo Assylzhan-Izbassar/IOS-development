831fecc274cf114a8c0d9e8563c7cf689eafd8a4
Assylzhan-Izbassar <asil_zhan.kz@mail.ru>
2021-02-02 23:02:21 +0600

    CalculatorApp

---
+++ Assignment-1/CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by macbook on 02.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myDisplay: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pressedDigit(_ sender: UIButton) {
+    }
+}
+
