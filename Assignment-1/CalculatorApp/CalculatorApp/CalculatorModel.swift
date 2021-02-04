dd3c80577d5365c897677ea35cd85c8e5cbd38c2
Assylzhan-Izbassar <asil_zhan.kz@mail.ru>
2021-02-05 00:44:01 +0600

    demo#1.2

--- Assignment-1/CalculatorApp/CalculatorApp/CalculatorModel.swift
+++ Assignment-1/CalculatorApp/CalculatorApp/CalculatorModel.swift
@@ -8,60 +8,109 @@
 import Foundation
 
 enum Operations {
-    case equals
-    case clear
-    case all_clear
-    case separator
-    case const(Double)
     case unaryOperation((Double) -> Double)
     case binaryOperation((Double, Double) -> Double)
+    case equals
+    case zero
+    case separator
+}
+
+enum CalcState : String {
+    case zero
+    case accumulate
+    case separate
+    case compute
+    case result
 }
 
 struct Calculator {
     
     struct SaveFirstOperandAndOperation {
-        var firstOperand: Double
-        var operation: (Double, Double) -> Double
+//        Variables
+        var firstOperand: Double?
+        var unaryOperation: ((Double) -> Double)?
+        var operation: ((Double, Double) -> Double)?
+        
+//        Constructors
+        init(){
+            
+        }
+        
+        init(fstOp op1: Double, func uOp: @escaping (Double) -> Double){
+            firstOperand = op1
+            unaryOperation = uOp
+        }
+        
+        init(fstOp op1: Double, func op: @escaping (Double,Double) -> Double){
+            firstOperand = op1
+            operation = op
+        }
+        
+//        Methods
+        func performUnaryOperation() -> (Double){
+            return unaryOperation!(firstOperand!)
+        }
         
         func performOperationWith(secondOperand op2: Double) -> Double{
-            return operation(firstOperand, op2)
+            return operation!(firstOperand!, op2)
         }
     }
     
     private var global_value: Double?
-    private var saving: SaveFirstOperandAndOperation?
+    private var saving: SaveFirstOperandAndOperation = SaveFirstOperandAndOperation()
     private var operations: Dictionary<String, Operations> =
         [
-            "+" : Operations.binaryOperation({$0+$1}),
-            "-" : Operations.binaryOperation({$0-$1}),
-            "x" : Operations.binaryOperation({$0*$1}),
-            "/" : Operations.binaryOperation({$0/$1}),
+            "+" : Operations.binaryOperation({$0 + $1}),
+            "-" : Operations.binaryOperation({$0 - $1}),
+            "x" : Operations.binaryOperation({$0 * $1}),
+            "/" : Operations.binaryOperation({$0 / $1}),
             "%" : Operations.unaryOperation({$0 / 100}),
-            "+/-": Operations.unaryOperation({(-1) * $0}),
+            "+/-": Operations.unaryOperation({$0 * (-1)}),
             "=" : Operations.equals,
-            "AC": Operations.all_clear,
-            "C" : Operations.clear,
+            "AC": Operations.zero,
             ".": Operations.separator
         ]
+    private var calcState: CalcState = CalcState.zero
     
-    mutating func setOperand(_ operand: Double) {
-        global_value = operand
+//  Methods
+    mutating func setOperand(_ operand: String) {
+        global_value = Double(operand)
+    }
+    
+    mutating func setCalcState(_ operand: String) {
+        if operand == "accumulate" {
+            calcState = CalcState.accumulate
+        }
     }
     
     mutating func performOperation(_ operation: String) {
         let symbol = operations[operation]!
         
         switch symbol {
+        
+        case .unaryOperation(let function):
+            saving.firstOperand = global_value!
+            saving.unaryOperation = function
+            global_value = saving.performUnaryOperation()
+            
         case .binaryOperation(let function):
-            saving = SaveFirstOperandAndOperation(firstOperand: global_value!, operation: function)
+            saving.firstOperand = global_value!
+            saving.operation = function
+            global_value = saving.performOperationWith(secondOperand: global_value!)
+            
         case .equals:
-            global_value = saving?.performOperationWith(secondOperand: global_value!)
+            global_value = saving.performOperationWith(secondOperand: global_value!)
+            
         default:
             break
         }
     }
     
-    func getResult() -> Double {
-        return global_value!
+    func getResult() -> String {
+        if(Int(global_value! * 10) % 10 == 0){
+            return String(Int(global_value!))
+        }else{
+            return String(global_value!)
+        }
     }
 }
--- Assignment-1/CalculatorApp/CalculatorApp/ViewController.swift
+++ Assignment-1/CalculatorApp/CalculatorApp/ViewController.swift
@@ -12,17 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var myDisplay: UILabel!
     private var model = Calculator()
     var typing: Bool = false
-    var displayValue: Double {
+    var displayValue: String {
         get {
-            return Double(myDisplay.text!)!
+            return myDisplay.text!
         }
         set {
-            myDisplay.text = String(newValue)
+            myDisplay.text = newValue
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        model.setOperand(displayValue)
         // Do any additional setup after loading the view.
     }
     
@@ -31,7 +32,12 @@ class ViewController: UIViewController {
         if typing {
             let current_display = myDisplay.text!
             myDisplay.text = current_display + current_digit
-        }else {
+            model.setCalcState("accumulate")
+        }else if(myDisplay.text! == "0" && current_digit == "0"){
+            myDisplay.text = current_digit
+            typing = false
+        }
+        else {
             myDisplay.text = current_digit
             typing = true
         }
