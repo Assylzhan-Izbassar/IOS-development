00e75f3085e78c4a6566d3581db6957f0b2ec5f9
Assylzhan-Izbassar <asil_zhan.kz@mail.ru>
2021-02-17 00:13:37 +0600

    paintApp_demo#0.1

---
+++ Assignment-3/PaintApp/PaintApp/CustomView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomView.swift
+//  PaintApp
+//
+//  Created by macbook on 17.02.2021.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+}
