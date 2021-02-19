de1c222373c18bdd6f7a5490950bff2f2e7e10f3
Assylzhan-Izbassar <asil_zhan.kz@mail.ru>
2021-02-19 18:26:39 +0600

    demo#1.1

--- Assignment-3/PaintApp/PaintApp/Canvas.swift
+++ Assignment-3/PaintApp/PaintApp/Canvas.swift
@@ -10,25 +10,40 @@ import UIKit
 class Canvas: UIView {
     
     @IBOutlet weak var isFilled: UISwitch!
+    @IBOutlet weak var undoBtn: UIButton!
     var model = PaintModel()
     
+    /**
+     Temporary fields
+     */
     var point1: CGPoint?
     var point2: CGPoint?
     var color: UIColor?
+    var isFirst: Bool = false
+    var prevState: formState? = formState.circle
+    var lineWidth: CGFloat?
     
     override func draw(_ rect: CGRect) {
+        if !isFirst {
+            initUndo()
+            isFirst = true
+        }
         // Drawing code
         if let p1 = point1 {
-            model.addFigure(p1, point2, isFilled.isOn, color ?? .systemIndigo)
-            model.drawAll()
+            model.addFigure(p1, point2, lineWidth ?? 3, isFilled.isOn, color ?? .systemIndigo)
         }
+        model.drawAll()
+        model.setState(prevState!)
     }
     
+    /**
+     Touch handlers
+     */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         point1 = touches.first?.location(in: self)
         
         if model.form == formState.pen {
-            model.figures?.append(Curve(color ?? .systemIndigo, 3, false))
+            model.figures?.append(Curve(color ?? .systemIndigo, lineWidth ?? 3, false))
         }
     }
     
@@ -51,23 +66,51 @@ class Canvas: UIView {
         }
     }
     
+    private func initUndo() {
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(long))
+        undoBtn.addGestureRecognizer(longGesture)
+    }
+    
+    /**
+     Undo actions
+     */
+    @IBAction func undo(_ sender: UIButton) {
+        prevState = model.form
+        model.setState(formState.undo)
+        setNeedsDisplay()
+    }
+
+    @objc func long() {
+        prevState = model.form
+        model.setState(formState.longUndo)
+        setNeedsDisplay()
+    }
+    
+    @IBAction func changeLineWidth(sender: UISlider) {
+        lineWidth = CGFloat(sender.value)
+    }
     /**
      Setting the form of figures
      */
     @IBAction func setCircle(_ sender: UIButton) {
         model.setState(formState.circle)
+        prevState = formState.circle
     }
     @IBAction func setRectangle(_ sender: UIButton) {
         model.setState(formState.rectangle)
+        prevState = formState.rectangle
     }
     @IBAction func setLine(_ sender: UIButton) {
         model.setState(formState.line)
+        prevState = formState.line
     }
     @IBAction func setTriangle(_ sender: UIButton) {
         model.setState(formState.triangle)
+        prevState = formState.triangle
     }
     @IBAction func setPen(_ sender: UIButton) {
         model.setState(formState.pen)
+        prevState = formState.pen
     }
     
     /**
--- Assignment-3/PaintApp/PaintApp/Models/PaintModel.swift
+++ Assignment-3/PaintApp/PaintApp/Models/PaintModel.swift
@@ -15,6 +15,8 @@ enum formState : String{
     case line
     case triangle
     case pen
+    case undo
+    case longUndo
 }
 
 struct PaintModel {
@@ -22,35 +24,42 @@ struct PaintModel {
     var figures: [Drawable]? = []
     private(set) var form: formState? = formState.circle
     
-    mutating func addFigure(_ p1: CGPoint?, _ point2: CGPoint?, _ isFilled: Bool, _ color: UIColor) {
+    mutating func addFigure(_ p1: CGPoint?, _ point2: CGPoint?, _ lineWidth: CGFloat, _ isFilled: Bool, _ color: UIColor) {
         var fig: Drawable? = nil
         switch form {
             case .circle:
-                fig = Circle(CGFloat(abs(p1!.x-point2!.x)/2), CGPoint(x: abs(point2!.x+p1!.x)/2, y: abs(point2!.y+p1!.y)/2), color, 3, isFilled)
+                fig = Circle(CGFloat(abs(p1!.x-point2!.x)/2), CGPoint(x: abs(point2!.x+p1!.x)/2, y: abs(point2!.y+p1!.y)/2), color, lineWidth, isFilled)
             case .rectangle:
-                fig = Rectangle(p1!, point2!, color, 3, isFilled)
+                fig = Rectangle(p1!, point2!, color, lineWidth, isFilled)
             case .line:
-                fig = Line(p1!, point2!, color, 3, false)
+                fig = Line(p1!, point2!, color, lineWidth, false)
             case .triangle:
-                fig = Triangle(p1!, point2!, color, 3, isFilled)
+                fig = Triangle(p1!, point2!, color, lineWidth, isFilled)
             case .pen:
                 break
+            case .undo:
+                _ = figures?.popLast()
+            case .longUndo:
+                figures?.removeAll()
             case .none:
-                break
+                break;
         }
         if let f = fig {
             figures?.append(f)
         }
     }
     
-    mutating func addFigure(_ color: UIColor) {
-        addFigure(nil, nil, false, color)
-    }
-    
     func drawAll() {
         for figure in figures! {
             figure.drawPath()
         }
+    }
+    
+    mutating func undo() {
+        _ = figures?.removeAll()
+    }
+    mutating func clear() {
+        self.figures?.removeAll()
     }
     
     mutating func setState(_ form: formState) {
