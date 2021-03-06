88098896a5491683fd3b70c3dc7ae6c02f9daacc
Assylzhan-Izbassar <asil_zhan.kz@mail.ru>
2021-03-06 21:01:52 +0600

    asgnmt#5_init

---
+++ Assignment-5/KBTUApp/KBTUApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KBTUApp
+//
+//  Created by macbook on 06.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
